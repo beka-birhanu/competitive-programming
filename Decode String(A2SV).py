04c38d3e6cf715879b0505aebe4b83ce4864f6a4
beka birhanu <119675042+beka-birhanu@users.noreply.github.com>
2023-07-10 16:32:59 +0300

    Create Decode String(A2SV).py

---
+++ Decode String(A2SV).py
@@ -0,0 +1,16 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stk = []
+        for S in s:
+            if S != ']':
+                stk.append(S)
+            else:
+                chrct = ''
+                while stk[-1] != '[':
+                    chrct = stk.pop() + chrct
+                stk.pop()
+                k = ''
+                while stk and stk[-1].isdigit():
+                    k = stk.pop() + k
+                stk.append(chrct*int(k))
+        return ''.join(stk)
