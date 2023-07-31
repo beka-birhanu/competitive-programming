51e3040ef8e0f93dee82d32a356b3481c974fc53
beka birhanu <119675042+beka-birhanu@users.noreply.github.com>
2023-07-31 09:20:45 +0300

    Create Shifting Letters II(A2SV).py

---
+++ Shifting Letters II(A2SV).py
@@ -0,0 +1,41 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        idx = [0]*(len(s)+1)
+        for i,j,d in shifts:
+            if d:
+                idx[i] += 1
+                idx[j + 1] -= 1
+
+            else:
+                idx[i] -= 1
+                idx[j + 1] += 1
+
+        s = list(s)
+        s[0] = chr((ord(s[0])-97 + idx[0])%26 + 97)
+        
+        for i in range(1,len(s)):
+            idx[i] += idx[i-1]
+            s[i] = chr((ord(s[i])-97 + idx[i])%26 + 97)
+
+        return "".join(s)
+
+    # better memory efficent
+    def shiftingLetters2(self, s: str, shifts: List[List[int]]) -> str:
+        idx = [0]*(len(s)+1)
+        for shift in shifts:
+            if shift[2]:
+                idx[shift[0]] += 1
+                idx[shift[1] + 1] -= 1
+
+            else:
+                idx[shift[0]] -= 1
+                idx[shift[1] + 1] += 1
+
+        s = list(s)
+        s[0] = chr((ord(s[0])-97 + idx[0])%26 + 97)
+        
+        for i in range(1,len(s)):
+            idx[i] += idx[i-1]
+            s[i] = chr((ord(s[i])-97 + idx[i])%26 + 97)
+
+        return "".join(s)
