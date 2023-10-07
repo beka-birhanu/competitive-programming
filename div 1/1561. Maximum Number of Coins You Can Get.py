6c036736d9431ac82d4cbc344aa9a054c2d00de5
beka birhanu <beka.b.atomsa@gmail.com>
2023-10-07 07:43:54 +0300

    Runtime Beats-95.89%of and Memory  Beats-63.14%

---
+++ div 1/1561. Maximum Number of Coins You Can Get.py
@@ -0,0 +1,9 @@
+# Runtime 502ms and Memory 28.64MB
+
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        piles.sort()
+        me = 0
+        for i in range(len(piles)//3,len(piles),2):
+            me += piles[i]
+        return me
