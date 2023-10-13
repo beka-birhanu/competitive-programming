4edf85eec2495d4c34c66302451203036e779389
beka birhanu <beka.b.atomsa@gmail.com>
2023-10-13 18:12:13 +0300

    Runtime

---
+++ div 1/1493. Longest Subarray of 1's After Deleting One Element.py
@@ -0,0 +1,13 @@
+# Runtime 420ms and Memory 19Mb
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        zeros = deque()
+        ans = 0
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] == 0:
+                zeros.append(j)
+                if len(zeros) > 1:
+                    i = zeros.popleft() + 1
+            ans = max(ans, j-i)
+        return ans
