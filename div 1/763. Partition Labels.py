4b2fbfabc46dbd67d692e3a73a86a84d5fcdf2d7
beka birhanu <beka.b.atomsa@gmail.com>
2023-10-13 17:37:46 +0300

    Runtime Beats-87% and Memory Beats-97.6%

---
+++ div 1/763. Partition Labels.py
@@ -0,0 +1,18 @@
+# Runtime 40ms and Memory 16Mb
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        right_most = {}
+        for i, c in enumerate(s):
+            right_most[c] = i
+        
+        ans = []
+        l = r = 0
+        for i, c in enumerate(s):
+            r = max(r, right_most[c])
+
+            if i == r:
+                ans.append(r - l + 1)
+                l = r + 1
+        
+        return ans
