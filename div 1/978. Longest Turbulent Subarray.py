448dfbc67feb85fd3d627f87eaf6d47dd38eef01
beka birhanu <beka.b.atomsa@gmail.com>
2023-10-17 21:54:41 +0300

    Runtime Beats 11.79% and Memory Beats 75.79%

---
+++ div 1/978. Longest Turbulent Subarray.py
@@ -0,0 +1,23 @@
+# Runtime 468 ms and Memory 20.6 MB
+class Solution:
+    def maxTurbulenceSize(self, arr: list[int]) -> int:
+        ans = 1
+        j = 0
+        last_opp = 0
+
+        for i in range(1,len(arr)):
+            if arr[i-1] > arr[i]:
+                if last_opp > 0:
+                    j = i-1
+                last_opp = 1
+            elif arr[i-1] < arr[i]:
+                if last_opp < 0:
+                    j = i-1
+                last_opp = -1
+            elif arr[i-1] == arr[i]:
+                j = i
+                last_opp = 0
+
+            ans = max(ans,i-j+1)
+
+        return ans
