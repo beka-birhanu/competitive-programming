7ecbf5ecc1f66b6ffcfa0613659b79dc7d67cf2c
beka birhanu <beka.b.atomsa@gmail.com>
2024-04-30 07:25:54 +0300

    Created 'Disjoint Sets Union 2' by A2SV Hub

---
+++ 30-Apr-2024/Disjoint Sets Union 2 136318.py
@@ -0,0 +1,36 @@
+# Problem: Disjoint Sets Union 2 - https://codeforces.com/edu/course/2/lesson/7/1/practice/contest/289390/problem/B
+
+def find(parents, i):
+    while parents[i] != i:
+        parents[i] = parents[parents[i]]
+        i = parents[i]
+
+    return i
+
+
+n, m = map(int, input().split())
+p = [i for i in range(n)]
+g = [[i+1, i+1, 1] for i in range(n)]
+
+for _ in range(m):
+    q = input()
+
+    if q[0] == "u":
+        _, x, y = q.split()
+        x = int(x)-1
+        y = int(y)-1
+
+        px = find(p, x)
+        py = find(p, y)
+        if px != py:
+            p[px] = py
+            g[py][0] = min(g[px][0], g[py][0])
+            g[py][1] = max(g[px][1], g[py][1])
+            g[py][2] += g[px][2]
+
+    else:
+        _, x = q.split()
+        x = int(x)-1
+        px = find(p, x)
+
+        print(*g[px])
