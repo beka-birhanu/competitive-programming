7073e334cec8a6d11725a3277707dbe30ded7d10
beka birhanu <beka.b.atomsa@gmail.com>
2024-05-01 18:40:55 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 01-May-2024/Cirno's Perfect Bitmasks Classroom 138428.py
@@ -0,0 +1,18 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+import math
+
+
+for _ in range(int(input())):
+    x = int(input())
+    if x == 1:
+        print(3)
+        continue
+    lg = math.log2(x)
+    if lg == lg//1:
+        print(x + 1)
+        continue
+
+    p = bin(x)[:1:-1].find('1')
+
+    print(2**p)
