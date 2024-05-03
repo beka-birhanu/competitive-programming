09fab87cce88d1660cb64a07822559c6a59423d5
beka birhanu <beka.b.atomsa@gmail.com>
2024-05-03 08:36:48 +0300

    Created 'Fedor and New Game' by A2SV Hub

---
+++ 03-May-2024/Fedor and New Game 139585.py
@@ -0,0 +1,14 @@
+# Problem: Fedor and New Game - https://codeforces.com/contest/467/problem/B
+
+n, m, k = map(int, input().split())
+p = []
+for _ in range(m):
+    p.append(int(input()))
+
+f = int(input())
+f_c = 0
+
+while p:
+    if bin(f^p.pop())[2:].count("1") <= k:
+        f_c += 1
+print(f_c)
