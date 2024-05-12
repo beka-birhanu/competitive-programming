d3e9bfee3161e97bbfe6e3ac0050052a9d26d01b
beka birhanu <beka.b.atomsa@gmail.com>
2024-05-12 16:01:05 +0300

    Created 'Largest Local Values in a Matrix' by A2SV Hub

---
+++ 12-May-2024/Largest Local Values in a Matrix 147433.py
@@ -0,0 +1,20 @@
+# Problem: Largest Local Values in a Matrix - https://leetcode.com/problems/largest-local-values-in-a-matrix/
+
+class Solution:
+    def largestLocal(self, grid):
+        n = len(grid)
+
+        for i in range(1, n - 1):
+            for j in range(1, n - 1):
+                temp = 0
+
+                for k in range(i - 1, i + 2):
+                    for l in range(j - 1, j + 2):
+                        temp = max(temp, grid[k][l])
+
+                grid[i - 1][j - 1] = temp
+
+        n = len(grid)
+        grid = [row[:n-2] for row in grid[:n-2]]
+
+        return grid
