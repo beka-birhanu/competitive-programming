2ce43e378c2348805449b83e8491bfe0fc8f071b
beka birhanu <beka.b.atomsa@gmail.com>
2024-05-15 07:56:02 +0300

    Created 'E - Consecutive Subarrays' by A2SV Hub

---
+++ A2SV G5 - Contest #16 15-May-2024/E - Consecutive Subarrays 150490.py
@@ -0,0 +1,20 @@
+# Problem: E - Consecutive Subarrays - https://codeforces.com/gym/523525/problem/E
+
+import heapq
+
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+p1 = sum(a)
+hp = []
+curr_sum = 0
+
+for i in range(n-1, 0, -1):
+    curr_sum += a[i]
+    heapq.heappush(hp, -curr_sum)
+
+for i in range(k-1):
+    p1 -= heapq.heappop(hp)
+
+print(p1)
