39f968c05b3bcce6f27531b03f76be334c5a8277
beka birhanu <beka.b.atomsa@gmail.com>
2024-05-29 07:43:16 +0300

    Created 'Longest Common Prefix' by A2SV Hub

---
+++ 29-May-2024/Longest Common Prefix 161422.py
@@ -0,0 +1,16 @@
+# Problem: Longest Common Prefix - https://leetcode.com/problems/longest-common-prefix/
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+
+        if len(strs) == 1:
+            return strs[0]
+        
+        strs.sort()
+        prefix = ""
+        for i in range(len(strs[0])):
+            if strs[0][i] == strs[-1][i]:
+                prefix += strs[0][i]
+            else:
+                break
+        return prefix
