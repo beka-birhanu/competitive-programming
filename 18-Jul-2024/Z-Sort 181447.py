058334d5a26519f893053f286a64a7e19724771a
beka birhanu <beka.b.atomsa@gmail.com>
2024-07-18 09:03:03 +0300

    Created 'Z-Sort' by A2SV Hub

---
+++ 18-Jul-2024/Z-Sort 181447.py
@@ -0,0 +1,9 @@
+# Problem: Z-Sort - https://codeforces.com/problemset/problem/652/B
+
+n = input()
+A = list(map(int, input().split()))
+
+for i in range(len(A) - 1):
+    A[i:i + 2] = sorted(A[i:i + 2], reverse=i % 2)
+
+print(*A)
