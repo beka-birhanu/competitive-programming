b0b7c764828bd6ac1641a9ecd25dd7e5d960254d
Beka B. Atomsa <beka.b.atomsa@gmail.com>
2024-08-21 07:27:44 +0300

    Created 'Maximum XOR for Each Query' by A2SV Hub

---
+++ 21-Aug-2024/Maximum XOR for Each Query 198347.py
@@ -0,0 +1,18 @@
+# Problem: Maximum XOR for Each Query - https://leetcode.com/problems/maximum-xor-for-each-query/
+
+class Solution:
+    def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+        _max = 2**maximumBit
+        xor = 0
+        for num in nums:
+            xor ^= num
+        
+        query_result = []
+        for num in reversed(nums):
+            inverse = ~xor
+            k = inverse % _max
+            
+            query_result.append(k)
+            xor ^= num
+        
+        return query_result
