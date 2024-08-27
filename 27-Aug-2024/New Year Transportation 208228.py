1d70a6eca096d09b99f1c3562ee9025dc7c96ff9
Beka B. Atomsa <beka.b.atomsa@gmail.com>
2024-08-27 07:38:40 +0300

    Created 'New Year Transportation' by A2SV Hub

---
+++ 27-Aug-2024/New Year Transportation 208228.py
@@ -0,0 +1,19 @@
+# Problem: New Year Transportation - https://codeforces.com/problemset/problem/500/A
+
+n, t = map(int, input().split())
+a = list(map(int, input().split()))
+a.append(0)
+
+st = [0]
+while st:
+    cur = st.pop()
+    if cur == t - 1:
+        print("YES")
+        break
+
+    if a[cur] != 0:
+        st.append(cur + a[cur])
+        a[cur] = 0
+
+else:
+    print("NO")
