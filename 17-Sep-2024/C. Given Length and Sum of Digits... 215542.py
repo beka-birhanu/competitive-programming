6ae9442feec0655faece55c75a40a094a0f5dde8
Beka B. Atomsa <beka.b.atomsa@gmail.com>
2024-09-17 06:38:54 +0300

    Created 'C. Given Length and Sum of Digits...' by A2SV Hub

---
+++ 17-Sep-2024/C. Given Length and Sum of Digits... 215542.py
@@ -0,0 +1,42 @@
+# Problem: C. Given Length and Sum of Digits... - https://codeforces.com/contest/489/problem/C
+
+def can(m, s):
+    return 0 <= s <= 9 * m
+
+
+def find_min_number(m, s):
+    _min = ""
+    for i in range(m):
+        for d in range(10):
+            if (i > 0 or d > 0 or (m == 1 and d == 0)) and can(m - i - 1, s - d):
+                _min += str(d)
+                s -= d
+                break
+
+    if not _min or len(str(int(_min))) < len(_min) or s > 0 or len(_min) != m:
+        return '-1'
+
+    else:
+        return _min if _min else '-1'
+
+
+def find_max_number(m, s):
+    _max = ""
+    for i in range(m):
+        for d in range(9, -1, -1):
+            if (i > 0 or d > 0 or (m == 1 and d == 0)) and can(m - i - 1, s - d):
+                _max += str(d)
+                s -= d
+                break
+
+    if not _max or len(str(int(_max))) < len(_max) or s > 0 or len(_max) != m:
+        return '-1'
+
+    else:
+        return _max
+
+
+m, s = map(int, input().split())
+_min = find_min_number(m, s)
+_max = find_max_number(m, s)
+print(_min, _max)
