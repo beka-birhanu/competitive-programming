b40d977686022015aa1ee4d357ab88cdf6b4f3f6
Beka B. Atomsa <beka.b.atomsa@gmail.com>
2024-09-18 06:45:36 +0300

    Created 'Maximal AND' by A2SV Hub

---
+++ 18-Sep-2024/Maximal AND 215706.py
@@ -0,0 +1,22 @@
+# Problem: Maximal AND - https://codeforces.com/problemset/problem/1669/H
+
+for _ in range(int(input())):
+    count = [0]*31
+    n, k = map(int, input().split())
+    for num in map(int, input().split()):
+        idx = 0
+        while num > 0:
+            if num & 1:
+                count[idx] += 1
+            num >>= 1
+            idx += 1
+
+    ans = 0
+    for i in range(30, -1, -1):
+        ans <<= 1
+        if n-count[i] <= k:
+            k -= (n-count[i])
+            count[i] = n
+        if count[i] == n:
+            ans |= 1
+    print(ans)
