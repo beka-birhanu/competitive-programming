c4a79634d87271869c1e43a83891c5553817ce3a
Beka B. Atomsa <beka.b.atomsa@gmail.com>
2024-10-04 07:34:54 +0300

    Created 'H-Index' by A2SV Hub

---
+++ 04-Oct-2024/H-Index 217937.py
@@ -0,0 +1,11 @@
+# Problem: H-Index - https://leetcode.com/problems/h-index/
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        greater = []
+        citations.sort(reverse=True)
+        for i in range(len(citations),-1,-1):
+            while citations and citations[0] >= i:
+                greater.append(citations.pop(0))
+            if len(greater) >= i:
+                return i
