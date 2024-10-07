758ca5d457b2d81e5e060e2c07ded996b252031f
Beka B. Atomsa <beka.b.atomsa@gmail.com>
2024-10-07 14:47:05 +0300

    Created 'Subdomain Visit Count' by A2SV Hub

---
+++ 07-Oct-2024/Subdomain Visit Count 218643.py
@@ -0,0 +1,15 @@
+# Problem: Subdomain Visit Count - https://leetcode.com/problems/subdomain-visit-count/description/
+
+class Solution:
+    def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        domains = defaultdict(int)
+        for cpdomain in cpdomains:
+            count,domain = cpdomain.split()
+            count = int(count)
+            domain = domain.split(".")
+            for i in range(len(domain)):
+                domains[".".join(domain[i:])] += count
+                
+        return[f"{count} {domain}" for domain,count in domains.items()]
+
+        
