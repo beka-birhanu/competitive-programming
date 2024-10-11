145978afd18a07114f5fc50a66f842082b3e6d5d
Beka B. Atomsa <beka.b.atomsa@gmail.com>
2024-10-11 07:49:45 +0300

    Created 'Minimal string' by A2SV Hub

---
+++ 11-Oct-2024/Minimal string 219379.py
@@ -0,0 +1,22 @@
+# Problem: Minimal string - https://codeforces.com/contest/797/problem/C
+
+s = input().strip()
+n = len(s)
+t = []
+u = []
+
+min_chars = [None] * n
+min_chars[-1] = s[-1]
+
+for i in range(n - 2, -1, -1):
+    min_chars[i] = min(s[i], min_chars[i + 1])
+
+i = 0
+while i < n or t:
+    while t and (i == n or t[-1] <= min_chars[i]):
+        u.append(t.pop())
+    if i < n:
+        t.append(s[i])
+        i += 1
+
+print("".join(u))
