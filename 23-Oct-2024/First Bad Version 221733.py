902eeb805547a1be0f38964f93ecef2aab58df94
Beka B. Atomsa <beka.b.atomsa@gmail.com>
2024-10-23 07:48:37 +0300

    Created 'First Bad Version' by A2SV Hub

---
+++ 23-Oct-2024/First Bad Version 221733.py
@@ -0,0 +1,18 @@
+# Problem: First Bad Version - https://leetcode.com/problems/first-bad-version/
+
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left = 1
+        right = n
+        
+        while left < right:
+            mid = (left+right)//2
+            if not isBadVersion(mid):
+                left = mid +1
+            else:
+                right = mid
+
+        return right
